982c40e149ec42c115982da43778a17dfe6624ec
aluvium <aluvium@protonmail.com>
2022-04-25 19:28:03 +0200

    octal function

---
+++ octa.py
@@ -0,0 +1,23 @@
+### Function octal -- > decimal -- > ASCI
+import os
+from check import fcheck
+def octa():
+    x=fcheck()
+    with open( x ,'r') as f:
+        e=f.read()
+        os.system('clear')
+        print("[[ ** Printing recived file ** ]]")
+        print(f'{e}\n')
+        d=e.split()
+        b=''
+        bb=''
+        dd=''
+        for each in d:
+             b=(int(each, base=8))       
+             bb+=str(b)+"  "
+             dd+=chr(b)
+        print("[[ ** Printing decoded file ** ]]")
+        print(f"Decimal -- > {bb}")
+        print(f'ASCII   -- > {dd}\n\n')    
+        f.close()
+    return None
